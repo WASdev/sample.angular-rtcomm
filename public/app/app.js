c5309e1d64babc30082e44a9e4804d20c9620ddd
Jose F. Martinez <jfmartin@us.ibm.com>
2016-01-06 16:32:47 -0500

    Disconnect is now disabled when the user is not registered

--- public/app/app.js
+++ public/app/app.js
@@ -10,41 +10,41 @@
     rtcommAppInit.$inject = ['rtcommConfig'];
 
     function rtcommAppInit(rtcommConfig) {
-
-      rtcommConfig.initialize();
+        rtcommConfig.initialize();
     }
 
     rtcommConfig.$inject = ['RtcommService', '$log'];
 
     function rtcommConfig(RtcommService, $log) {
         var service = {
-            initialize : initialize
+            initialize: initialize
         };
 
         return service;
         /////////////////////////
 
-        function initialize(){
-          var config = {
-              "server": window.location.hostname || 'localhost',
-              "port": window.location.port || 80,
-              "rtcommTopicPath": "/rtcomm/",
-              "createEndpoint": true,
-              "presenceTopic": "sampleRoom",
-              "userid": ""
-          };
+        function initialize() {
+            var config = {
+                "server": window.location.hostname || 'localhost',
+                "port": window.location.port || 80,
+                "rtcommTopicPath": "/rtcomm/",
+                "presenceTopic": "sampleRoom",
+                "userid": ""
+            };
 
-          RtcommService.setConfig(config);
+            //Set configuration
+            RtcommService.setConfig(config);
 
         }
     }
 
-    SessionController.$inject = ['RtcommService', '$log'];
+    SessionController.$inject = ['$scope', 'RtcommService', '$log'];
 
-    function SessionController(RtcommService, $log) {
+    function SessionController($scope, RtcommService, $log) {
         var session = this;
         var endpoint;
 
+        session.connected = false;
         session.disconnect = disconnect;
 
         function disconnect() {
@@ -54,5 +54,15 @@
             endpoint.disconnect();
             $log.debug('sample-app - Disconnected.');
         }
+
+        //Listen for event broadcasted when rtcomm endpoint is initialized (registered)
+        $scope.$on('rtcomm::init', function(event, success, details){
+            if(success == true){
+              session.connected = true;
+            }
+            else{
+              session.connected = false;
+            }
+        })
     }
 })();
